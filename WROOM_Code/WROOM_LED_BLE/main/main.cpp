f482e78ee823c81005381d4e38d580070da96648
Bailey Mosher <baileymosher22@gmail.com>
2024-03-20 14:09:29 -0400

    WROOM_Code Initial Commit, build delete in LED_Matrix

---
+++ WROOM_Code/WROOM_LED_BLE/main/main.cpp
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+// #include "esp32-hal.h"
+
+
+  /*
+  Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+  Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+  Ported to Arduino ESP32 by Evandro Copercini
+
+  Create a BLE server that, once we receive a connection, will send periodic notifications.
+  The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+  Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
+  Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+
+  The design of creating the BLE server is:
+  1. Create a BLE Server
+  2. Create a BLE Service
+  3. Create a BLE Characteristic on the Service
+  4. Create a BLE Descriptor on the characteristic
+  5. Start the service.
+  6. Start advertising.
+
+  In this example rxValue is the data received (only accessible inside that function).
+  And txValue is the data to be sent, in this example just a byte incremented every second. 
+*/
+
+#include "BLEDevice.h"
+#include "BLEServer.h"
+#include "BLEUtils.h"
+#include "BLE2902.h"
+
+/********************************
+ * UUID DEFINES
+ *******************************/
+  // See the following for generating UUIDs:
+  // https://www.uuidgenerator.net/
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+/********************************
+ * VARIABLE DEFINITIONS
+ *******************************/
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+
+/********************************
+ * MAIN-CONTAINED BLE CLASS
+ *******************************/
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      String rxValue = pCharacteristic->getValue();
+      int rxLen = rxValue.length();
+
+      // if (rxValue.length() > 0) {
+      //   Serial.println("*********");
+      //   Serial.print("Received Value: ");
+
+      //   for (int i = 0; i < rxValue.length(); i++) {
+      //     Serial.print(rxValue[i]);
+      //   }
+        
+      //   Serial.println();
+      //   Serial.println("*********");
+      // }
+      // Use ESP_LOGE for now...
+      if (rxLen > 0) {
+        const char* rxValueCharArr = rxValue.c_str();
+        ESP_LOGI("RXValue", "*********");
+        ESP_LOGI("RXValue", "Received Value: %s", rxValueCharArr);
+        ESP_LOGI("RXValue", "*********");
+      }
+    }
+};
+
+
+/********************************
+ * TASK HANDLING
+ *******************************/
+static TaskHandle_t s_rx_to_serial_handle = NULL;  /* handle of serial task  */
+
+static void rx_to_serial_handler(void *arg) {
+  for(;;) {
+    // Delay until next cycle
+    vTaskDelay(1 / portTICK_PERIOD_MS);
+  }
+}
+
+/********************************
+ * Main Entry Point
+ *******************************/
+extern "C" void app_main(void)
+{
+  initArduino();
+  // Create the task that handles BLE interactions
+  // xTaskCreate(rx_to_serial_handler, "RXValueTask", 3072, NULL, 2, &s_rx_to_serial_handle);
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32_BLE"); // Give it a name
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_TX,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+                      
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                        CHARACTERISTIC_UUID_RX,
+                                        BLECharacteristic::PROPERTY_WRITE
+                                      );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  ESP_LOGI("BLEClient", "Waiting on a client connection to notify...");
+}
