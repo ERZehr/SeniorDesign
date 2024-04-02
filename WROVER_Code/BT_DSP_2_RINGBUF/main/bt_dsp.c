7de1cf5c10f915f6ed8e7e68d86350ee7e17a3c4
Bailey Mosher <baileymosher22@gmail.com>
2024-04-02 16:10:06 -0400

    One-filter BT/DSP

--- WROVER_Code/BT_DSP_2_RINGBUF/main/bt_dsp.c
+++ WROVER_Code/BT_DSP_2_RINGBUF/main/bt_dsp.c
@@ -2,30 +2,52 @@
 /*******************************
  * STATIC VARIABLE DEFINITIONS
  ******************************/
+// static int16_t sample_l = 0;
+// static int16_t sample_r = 0;
+// static float sample_l_f = 0.0f;
+// static float sample_r_f = 0.0f;
+// static float a0[BAND_MAX] = {1.0037, 1.0036, 1.0071};
+// static float a1[BAND_MAX] = {-2,-1.9999, -1.9996};
+// static float a2[BAND_MAX] = {0.9963, 0.9964, 0.9929};
+// static float b0[BAND_MAX] = {0.0037, 0.0036, 0.0071};
+// static float b2[BAND_MAX] = {-0.0037, -0.0036, -0.0071};
+// static float y_l[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
+// static float y1_l[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
+// static float y2_l[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
+// static int16_t x0_l = 0;
+// static int16_t x1_l = 0;
+// static int16_t x2_l = 0;
+// static float y_r[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
+// static float y1_r[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
+// static float y2_r[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
+// static int16_t x0_r = 0;
+// static int16_t x1_r = 0;
+// static int16_t x2_r = 0;
+
+// // USER MODIFIED - see update_multipliers function for details
+// static float multipliers[BAND_MAX] = {1,1,0,0,0,0,0,0,0,0,0,0};
+
 static int16_t sample_l = 0;
 static int16_t sample_r = 0;
 static float sample_l_f = 0.0f;
 static float sample_r_f = 0.0f;
-static float a0[BAND_MAX] = {1.0037, 1.0036, 1.0071};
-static float a1[BAND_MAX] = {-2,-1.9999, -1.9996};
-static float a2[BAND_MAX] = {0.9963, 0.9964, 0.9929};
-static float b0[BAND_MAX] = {0.0037, 0.0036, 0.0071};
-static float b2[BAND_MAX] = {-0.0037, -0.0036, -0.0071};
-static float y_l[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
-static float y1_l[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
-static float y2_l[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
+static float a0 = 1.0037;
+static float a1 = -2;
+static float a2 = 0.9963;
+static float b0 = 0.0037;
+static float b2 = -0.0037;
+static float y_l = 0;
+static float y1_l = 0;
+static float y2_l = 0;
 static int16_t x0_l = 0;
 static int16_t x1_l = 0;
 static int16_t x2_l = 0;
-static float y_r[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
-static float y1_r[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
-static float y2_r[BAND_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0};
+static float y_r = 0;
+static float y1_r = 0;
+static float y2_r = 0;
 static int16_t x0_r = 0;
 static int16_t x1_r = 0;
 static int16_t x2_r = 0;
-
-// USER MODIFIED - see update_multipliers function for details
-static float multipliers[BAND_MAX] = {1,1,0,0,0,0,0,0,0,0,0,0};
 
 
 // // Int only
@@ -84,55 +106,50 @@ bool bt_media_biquad_bilinear_filter(uint8_t *media, uint32_t len) {
         sample_r_f = (float)sample_r;
 
         
-        outBuf[i + 1] = (uint8_t) ((sample_l >> 8) & 0xff);
-        outBuf[i] = (uint8_t) (0xff & sample_l);
-        outBuf[i + 3] = (uint8_t) ((sample_r >> 8) & 0xff);
-        outBuf[i + 2] = (uint8_t) (0xff & sample_r);
+        // outBuf[i + 1] = (uint8_t) ((sample_l >> 8) & 0xff);
+        // outBuf[i] = (uint8_t) (0xff & sample_l);
+        // outBuf[i + 3] = (uint8_t) ((sample_r >> 8) & 0xff);
+        // outBuf[i + 2] = (uint8_t) (0xff & sample_r);
 
         // Inner loop generates coefficients for each band
-        // for(uint8_t z = 0; z < 1; z++) {
-        //     // Re-initialize floats with any changes to samples
-        //     sample_l_f = (float)sample_l / 0x8000;
-        //     sample_r_f = (float)sample_r / 0x8000;
+        // for(uint8_t z = 0; z < BAND_MAX; z++) {
+            // Re-initialize floats with any changes to samples
 
-        //     // Coefficient generation for each iteration - left channel
-        //     y2_l[z] = y1_l[z];
-        //     y1_l[z] = y_l[z];
-        //     x2_l = x1_l;
-        //     x1_l = x0_l;
-        //     x0_l = sample_l_f;
-        //     y_l[z] = (b0[z]*x0_l + b2[z]*x2_l - (a1[z]*y1_l[z] + a2[z]*y2_l[z])) / a0[z];
-            
-        //     // Coefficient generation for each iteration - right channel
-        //     y2_r[z] = y1_r[z];
-        //     y1_r[z] = y_r[z];
-        //     x2_r = x1_r;
-        //     x1_r = x0_r;
-        //     x0_r = sample_r_f;
-        //     y_r[z] = (b0[z]*x0_r + b2[z]*x2_r - (a1[z]*y1_r[z] + a2[z]*y2_r[z])) / a0[z];
+        // Coefficient generation for each iteration - left channel
+        y2_l = y1_l;
+        y1_l = y_l;
+        x2_l = x1_l;
+        x1_l = x0_l;
+        x0_l = sample_l_f;
+        y_l = (b0*x0_l + b2*x2_l - (a1*y1_l + a2*y2_l)) / a0;
+        
+        // Coefficient generation for each iteration - right channel
+        y2_r = y1_r;
+        y1_r = y_r;
+        x2_r = x1_r;
+        x1_r = x0_r;
+        x0_r = sample_r_f;
+        y_r = (b0*x0_r + b2*x2_r - (a1*y1_r + a2*y2_r)) / a0;
 
-        //     // Audio processing - left channel
-        //     sample_l_f -= multipliers[z] * y_l[z];
-        //     // Clamp audio between uint16_t max limits - strictly in range [0, 65534]
-        //     if(sample_l_f > 32767) {sample_l_f = 32767;}
-        //     if(sample_l_f < -32768) {sample_l_f = -32768;}
-        //     // Convert back to int and place in output buffer
-        //     sample_l = (int16_t) sample_l_f;
-        //     outBuf[i + 1] = (uint8_t) ((sample_l >> 8) & 0xff);
-        //     outBuf[i] = (uint8_t) (0xff & sample_l);
-        //     // // Mono
-        //     // outBuf[i + 3] = (uint8_t) ((sample_l >> 8) & 0xff);
-        //     // outBuf[i + 2] = (uint8_t) (0xff & sample_l);
+        // Audio processing - left channel
+        sample_l_f = y_l;
+        // Clamp audio between uint16_t max limits - strictly in range [0, 65534]
+        if(sample_l_f > 32767) {sample_l_f = 32767;}
+        if(sample_l_f < -32768) {sample_l_f = -32768;}
+        // Convert back to int and place in output buffer
+        sample_l = (int16_t) sample_l_f;
+        outBuf[i + 1] = (uint8_t) ((sample_l >> 8) & 0xff);
+        outBuf[i] = (uint8_t) (0xff & sample_l);
 
-        //     // Audio processing - right channel
-        //     sample_r_f -= multipliers[z] * y_r[z];
-        //     // Clamp audio between uint16_t max limits - strictly in range [0, 65534]
-        //     if(sample_r_f > 32767) {sample_r_f = 32767;}
-        //     if(sample_r_f < -32768) {sample_r_f = -32768;}
-        //     // Convert back to int and place in output buffer
-        //     sample_r = (int16_t) sample_r_f;
-        //     outBuf[i + 3] = (uint8_t) ((sample_r >> 8) & 0xff);
-        //     outBuf[i + 2] = (uint8_t) (0xff & sample_r);
+        // Audio processing - right channel
+        sample_r_f = y_r;
+        // Clamp audio between uint16_t max limits - strictly in range [0, 65534]
+        if(sample_r_f > 32767) {sample_r_f = 32767;}
+        if(sample_r_f < -32768) {sample_r_f = -32768;}
+        // Convert back to int and place in output buffer
+        sample_r = (int16_t) sample_r_f;
+        outBuf[i + 3] = (uint8_t) ((sample_r >> 8) & 0xff);
+        outBuf[i + 2] = (uint8_t) (0xff & sample_r);
         // }
     }
 
@@ -197,7 +214,7 @@ bool bt_media_biquad_bilinear_filter(uint8_t *media, uint32_t len) {
 }
 
 void update_multipliers(uint8_t *mults) {
-    for(uint8_t i = 0; i < BAND_MAX; i++) {
-        multipliers[i] = mults[i];
-    }
+    // for(uint8_t i = 0; i < BAND_MAX; i++) {
+    //     multipliers[i] = mults[i];
+    // }
 }
