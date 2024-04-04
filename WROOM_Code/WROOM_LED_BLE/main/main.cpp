b2700f70db667750b66c642e1e8f365cd090efff
Bailey Mosher <baileymosher22@gmail.com>
2024-04-04 12:44:00 -0400

    BLE Advertising Fix, WROVER DSP testing

--- WROOM_Code/WROOM_LED_BLE/main/main.cpp
+++ WROOM_Code/WROOM_LED_BLE/main/main.cpp
@@ -76,7 +76,6 @@
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
 /********************************
  * ADC DEFINES
@@ -142,6 +141,8 @@ class MyServerCallbacks: public BLEServerCallbacks {
 
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
+      // Start advertising
+      pServer->getAdvertising()->start();
     }
 };
 
@@ -269,7 +270,6 @@ extern "C" void app_main(void)
 
   // Create the BLE Device
   BLEDevice::init("ESP32_BLE"); // Give it a name
-  // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
   // Create the BLE Service
