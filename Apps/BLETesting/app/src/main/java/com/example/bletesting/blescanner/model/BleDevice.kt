755196b0f66f4a7b008139cd96647cbcfd385334
ERZehr <139022059+ERZehr@users.noreply.github.com>
2024-04-22 08:51:57 -0400

    Delete Apps directory

--- Apps/BLETesting/app/src/main/java/com/example/bletesting/blescanner/model/BleDevice.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bletesting.blescanner.model
-
-data class BleDevice(val name: String) {
-    companion object {
-        fun createBleDevicesList(): MutableList<BleDevice> {
-            return mutableListOf()
-        }
-    }
-}
